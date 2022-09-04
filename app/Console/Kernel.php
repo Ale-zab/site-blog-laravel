0d55002a540d8253ddd73e8773bffcfeef58d437
Aleksey Zabolotnikov <ale-zab@yandex.ru>
2022-09-04 13:12:37 +0000

    Fix errors

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -11,7 +11,7 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('article:send')->weeklyOn(1, '13:00');;
+         $schedule->command('article:send')->weeklyOn(1, '13:00');
     }
 
     /**
